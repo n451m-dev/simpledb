7f43159a3a9128e553251d35f560a83fb4606bd8
Nasim Ahmad <khannasimahmad@gmail.com>
2024-11-29 08:49:58 +0530

    used refelction and created connect method

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import grpc from "@grpc/grpc-js";
-import protoLoader from "@grpc/proto-loader";
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
 import path from "path";
-import DatabaseInterface from "./db-interface";
-
+import { DatabaseInterface } from "./database/db-interface";
+import { ReflectionService } from '@grpc/reflection';
 // Load proto file
-const PROTO_PATH = path.join(__dirname, "../../proto", "database.proto");
-const packageDefinition = protoLoader.loadSync(PROTO_PATH);
-const proto = grpc.loadPackageDefinition(packageDefinition) as any; // Cast to any to avoid type issues
+const PROTO_PATH = path.join(__dirname, "proto", "database.proto");
 
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const proto = grpc.loadPackageDefinition(packageDefinition) as any; // Cast to avoid type issues
+// Create the reflection implementation based on your gRPC package and add it to your server
+const reflection = new ReflectionService(packageDefinition);
 // Ensure you're accessing the correct service definition
 const DatabaseService = proto.database.DatabaseService;
 
@@ -16,6 +18,17 @@ const dbInterface = new DatabaseInterface();
 
 // Implement the gRPC server methods
 const serverImpl = {
+    async Connect(call: grpc.ServerUnaryCall<any, any>, callback: grpc.sendUnaryData<any>) {
+        try {
+            // Simulate a connection or any database check
+            console.log("Attempting to connect to the database...");
+            const connectionMessage = "Successfully connected to the database!";
+            callback(null, { message: connectionMessage });
+        } catch (err) {
+            callback({ code: grpc.status.INTERNAL, details: "Failed to connect to the database" }, null);
+        }
+    },
+
     async CreateCollection(call: grpc.ServerUnaryCall<any, any>, callback: grpc.sendUnaryData<any>) {
         try {
             const collectionName = call.request.collectionName;
@@ -79,8 +92,11 @@ const serverImpl = {
 // Create and start the gRPC server
 function startServer() {
     const server = new grpc.Server();
+   
+    // ReflectionService.enableReflection(server, DatabaseService.service);
     if (DatabaseService) {
         server.addService(DatabaseService.service, serverImpl);
+        reflection.addToServer(server);
     } else {
         console.error("DatabaseService not found in the loaded proto");
         return;
@@ -89,6 +105,7 @@ function startServer() {
     const PORT = "50051";
     server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), () => {
         console.log(`Server running on port ${PORT}`);
+        // server.start();
     });
 }
 
