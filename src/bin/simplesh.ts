1628c014d2ab3ab039cb71cc72aa6d86c4df3a5e
Nasim Ahmad <khannasimahmad@gmail.com>
2024-12-03 20:31:23 +0530

    created bin folder for executable file

---
+++ src/bin/simplesh.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import readline from 'readline';
+import { performOperation } from '../simplesh/perform-operation';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'simplesh> ',
+    terminal: false // Disables input echoing
+});
+
+// Manually write the prompt
+process.stdout.write('simplesh> ');
+
+// Use an async wrapper to handle the line event
+rl.on('line', async (line: string) => {
+    const input = line.trim();
+    if (input) {
+        try {
+            const output = await performOperation(input);
+            console.log(output);
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+        }
+    }
+    // Write the prompt manually
+    process.stdout.write('simplesh> ');
+});
+
+rl.on('close', () => {
+    console.log('simplesh exited.');
+    process.exit(0);
+});
