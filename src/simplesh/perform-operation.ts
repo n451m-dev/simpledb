1fce884a17caa369970d3125946c96d878d14965
Nasim Ahmad <khannasimahmad@gmail.com>
2024-12-14 23:53:35 +0530

    reverted back to use if else

--- src/simplesh/perform-operation.ts
+++ src/simplesh/perform-operation.ts
@@ -1,31 +1,31 @@
 import { parseQuery } from "./parse-query";
-import { createCollection, deleteCollection, listCollections, createDocument, deleteDocument, findOneDocument, findAllDocuments } from "./make-db-request";
+import { createCollection, createDocument, deleteCollection, deleteDocument, findAllDocuments, findOneDocument, listCollections } from "./make-db-request";
 
 export const performOperation = async (input: string): Promise<any> => {
     try {
+
         const { collection, method, args } = parseQuery(input);
+        // console.log("collection, method, args", collection, method, args);
 
-        // Handling collection-related methods
-        switch (`${collection}-${method}`) {
-            case 'collection-create':
-                return await createCollection(args);
-            case 'collection-listCollection':
-                return await listCollections();
-            case 'collection-delete':
-                return await deleteCollection(args);
-            case 'document-createOne':
-                return await createDocument(collection, args);
-            case 'document-deleteOne':
-                return await deleteDocument(collection, args);
-            case 'document-findOne':
-                return await findOneDocument(collection, args);
-            case 'document-find':
-                return await findAllDocuments(collection, args);
-            default:
-                throw Error('Incorrect Command');
+        // collection related methods
+        if (collection == 'collection' && method === 'create') {
+            return await createCollection(args);
+        } else if (collection == 'collection' && method === 'listCollection') {
+            return await listCollections();
+        } else if (collection === 'collection' && method === 'delete') {
+            return await deleteCollection(args)
+        } else if (method === 'createOne') {
+            return await createDocument(collection, args)
+        } else if (method === 'deleteOne') {
+            return await deleteDocument(collection, args)
+        } else if (method === 'findOne') {
+            return await findOneDocument(collection, args)
+        } else if (method === 'find') {
+            return await findAllDocuments(collection, args)
+        } else{
+            throw Error('Incorrect Command')
         }
-
     } catch (err) {
-        throw err;
+        throw err
     }
 };
