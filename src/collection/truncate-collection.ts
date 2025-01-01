46fb4094dfc77cdbd18c93aa15801559acefbbc9
Nasim Ahmad <khannasimahmad@gmail.com>
2025-01-01 16:00:13 +0530

    fixed truncate function and deleted the iterator function and used the refactored iterator function

--- src/collection/truncate-collection.ts
+++ src/collection/truncate-collection.ts
@@ -1,30 +1,6 @@
 import { LevelUp } from 'levelup';
-
-/**
- * Async generator to iterate over a LevelDB iterator.
- * @param iterator - The LevelDB iterator.
- */
-async function* asyncIterator(iterator: any): AsyncGenerator<string> {
-    try {
-        while (true) {
-            const result = await new Promise<{ key?: string }>((resolve, reject) => {
-                iterator.next((err: Error | null, key: string) => {
-                    if (err) return reject(err);
-                    resolve({ key });
-                });
-            });
-
-            if (!result.key) break; // End of iteration
-            yield result.key;
-        }
-    } catch (err) {
-        console.error("Iterator Error:", err);
-        throw err; // Re-throw the error for outer handling
-    } finally {
-        iterator.end(() => { });
-    }
-}
-
+import { asyncIterator } from '../helper/collection-iterator';
+import { findCollection } from './find-collection';
 /**
  * Deletes a collection and its associated data from the database.
  * @param db - The LevelDB instance.
@@ -32,26 +8,27 @@ async function* asyncIterator(iterator: any): AsyncGenerator<string> {
  */
 export async function truncateCollection(db: LevelUp, collectionName: string) {
     try {
-        if (!collectionName || typeof collectionName !== 'string') {
-            throw new Error('Collection name must be a non-empty string.');
-        }
-
-        const collectionKey = `__collection:${collectionName}`;
-        const exists = await db.get(collectionKey).catch((err) => (err.notFound ? false : Promise.reject(err)));
-        if (!exists) {
-            throw new Error(`Collection "${collectionName}" does not exist.`);
-        }
 
         const iterator = db.iterator({
             gte: `${collectionName}:`,
             lte: `${collectionName}:\xFF`,
         });
 
+        if (!collectionName || typeof collectionName !== 'string') {
+            throw new Error('Collection name must be a non-empty string.');
+        }
+
+        
+        const exists = await findCollection(db, collectionName)
+        if (!exists) {
+            throw new Error(`Collection "${collectionName}" does not exist.`);
+        }
+
         // Create a batch instance
         const batch = db.batch();
 
         // Add delete operations to the batch
-        for await (const key of asyncIterator(iterator)) {
+        for await (const [key, value] of asyncIterator(iterator)) {
             batch.del(key);
         }
 
